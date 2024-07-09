e1d04df0c087af830d62ee1f4f2fde8364e6cd06
limiquan <yiquan.1126@foxmail.com>
2024-07-09 16:50:39 +0800

    test

---
+++ album/photo/admin.py
@@ -0,0 +1,8 @@
+
+# photo/admin.py
+
+from django.contrib import admin
+from photo.models import Photo
+
+
+admin.site.register(Photo)
---
+++ album/photo/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from photo.views import home, upload
+
+# App名称
+# 用于Django幕后的url查询
+app_name = 'photo'
+
+# url列表
+urlpatterns = [
+    path('', home, name='home'),
+    path('upload/', upload, name='upload'),
+]
